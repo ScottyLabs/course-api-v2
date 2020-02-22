830a3e3f433a6ef2e06ede2769fe1bfe49443632
David Ethan Hwang <hdavidethan@gmail.com>
2020-02-22 17:10:38 -0500

    Uploaded courses to db

--- src/controllers/courseController.js
+++ src/controllers/courseController.js
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
 import { courseSchema } from '../models/courseModel';
+import fs from 'fs';
 
 const Course = mongoose.model('Course', courseSchema);
 
@@ -17,6 +18,50 @@ export const addCourse = (req, res) => {
             });
         }
     })
+}
+
+export const addCoursesFromJSON = (req, res) => {
+    let data = fs.readFileSync('./data/' + req.body.file);
+    let parsed = JSON.parse(data);
+    let result = {
+        added: [],
+        exists: []
+    }
+    for (var course in parsed.courses) {
+        if (parsed.courses[course].prereqs_obj.reqs_list != null) {
+            var prereqs = parsed.courses[course].prereqs_obj.reqs_list[0];
+        } else {
+            var prereqs = null;
+        }
+        if (parsed.courses[course].coreqs_obj.reqs_list != null) {
+            var coreqs = parsed.courses[course].coreqs_obj.reqs_list[0];
+        } else {
+            var coreqs = null;
+        }
+        let courseObj = {
+            courseID: course,
+            desc: parsed.courses[course].desc,
+            prereqs: prereqs,
+            coreqs: coreqs,
+            name: parsed.courses[course].name,
+            units: parsed.courses[course].units,
+            department: parsed.courses[course].department,
+            lectures: parsed.courses[course].lectures,
+            sections: parsed.courses[course].sections
+        }
+        
+        Course.find({ courseID: courseObj.courseID }, (err, docs) => {
+            if (docs.length == 0) {
+                let newCourse = new Course(courseObj);
+                newCourse.save((err, course) => {
+                    if (err) {
+                        res.send(err);
+                    }
+                })
+            }
+        })
+    }
+    res.json({ message: 'Successfully added documents' });
 }
 
 export const updateCourse = (req, res) => {
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,5 +1,9 @@
-import { addCourse, updateCourse } from '../controllers/courseController';
-import parser from '../api/parser';
+import { 
+    addCourse, 
+    addCoursesFromJSON, 
+    updateCourse 
+} from '../controllers/courseController';
+// import parser from '../api/parser';
 
 const routes = (app) => {
     app.route('/query')
@@ -12,11 +16,14 @@ const routes = (app) => {
             var semester = req.params.semester;
             console.log("Requested semester: " + semester);
             res.writeHead(200, {"content-type": "application/json"});
-            res.end(parser.getSemesterData(semester));
-        })
+            // res.end(parser.getSemesterData(semester));
+        });
 
     app.route('/update')
         .post(addCourse);
+    
+    app.route('/upload')
+        .post(addCoursesFromJSON);
     
     app.route('/update/:courseID')
         .put(updateCourse);
