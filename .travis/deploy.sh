188aca8239c48d8b6f9ed8d1d65648a310073e9e
David Ethan Hwang <hdavidethan@gmail.com>
2020-04-18 15:17:38 -0400

    Updated deploy setup

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 set -e
-ssh git@chicago.club.cc.cmu.edu -v exit
+git config --global push.default simple
+git remote add production ssh://git@chicago.club.cc.cmu.edu:deploy/course-api.git
+git push production master
