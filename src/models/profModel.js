a17b95c22f46a8923b4d94090023bf4cc7bab7fe
Alyssa Lee <alyssalee@MacBook-Pro.local>
2021-11-06 15:27:07 -0400

    Added profModel

---
+++ src/models/profModel.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+export const profSchema = new mongoose.Schema({
+  instructor: String,
+  andrewID: String,
+  courses: [
+    {
+      courseID: String, 
+      hrsPerWeek: Number,
+      teachingRating: Number,
+      overallRating: Number,
+      semesters: [{year: String, semester: String}]
+    }],
+  respectForStudents: Number,
+  interestInLearning: Number,
+  providesFeedback: Number,
+})
