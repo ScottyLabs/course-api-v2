191b0f01a84e7df27b654b5ded77ce38d476e0e0
Alyssa Lee <alyssalee@MacBook-Pro.local>
2021-11-06 15:12:30 -0400

    Updated prof schema

--- src/models/profModel.js
+++ src/models/profModel.js
@@ -3,5 +3,15 @@ import mongoose from "mongoose";
 export const profSchema = new mongoose.Schema({
   instructor: String,
   andrewID: String,
-  courses: [{courseID: String, year: String, semester: String}]
+  courses: [
+    {
+      courseID: String, 
+      hrsPerWeek: Number,
+      teaching: Number,
+      overall: Number,
+      semesters: [{year: String, semester: String}]
+    }],
+  respectForStudents: Number,
+  interestInLearning: Number,
+  providesFeedback: Number,
 })
