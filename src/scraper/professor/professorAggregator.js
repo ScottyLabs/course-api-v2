9bf6a33189ced3483955fffa273ddfdf58cb6f82
Knaoinr <knaoinr@gmail.com>
2021-11-13 16:35:27 -0500

    Working professor search aggregator

--- src/scraper/professor/professorAggregator.js
+++ src/scraper/professor/professorAggregator.js
@@ -5,12 +5,12 @@ const FCE = mongoose.model("FCE", fceSchema);
 
 let ratings = FCE.aggregate([
     {
-        $group: { //test
+        $group: {
             _id: "$instructor",
             andrewID: {$first: "$andrewID"},
-            respectForStudents: { $avg: { $arrayElemAt: [ "$rating", 6 ] }},
-            interestInLearning: { $avg: { $arrayElemAt: [ "$rating", 0 ] }},
-            providesFeedback: { $avg: { $arrayElemAt: [ "$rating", 2 ] }},
+            respectForStudents: {$avg: {$arrayElemAt: ["$rating", 6]}},
+            interestInLearning: {$avg: {$arrayElemAt: ["$rating", 0]}},
+            providesFeedback: {$avg: {$arrayElemAt: ["$rating", 2]}},
             courseInfo: {$push: {
                 courseID: "$courseID",
                 year: "$year",
@@ -19,34 +19,46 @@ let ratings = FCE.aggregate([
                 rating7: {$arrayElemAt: ["$rating", 7]},
                 rating8: {$arrayElemAt: ["$rating", 8]}
             }}
+        }
     },
-    { 
-    $group: {
-        _id: { instructor: "$instructor", courseID: "$courseID" },
-        andrewID: {$first: "$andrewID"},
-        hrsPerWeek: {$avg: "$hrsPerWeek"},
-        teachingRating: { $avg: { $arrayElemAt: [ "$rating", 7 ] } },
-        overallRating: {$avg: { $arrayElemAt: [ "$rating", 8 ] } },
-        semesters: {$push: {year: "$year", semester: "$semester"}},
-        rating0: {$push: {$arrayElemAt: ["$rating", 0]}},
-        rating2: {$push: {$arrayElemAt: ["$rating", 2]}},
-        rating6: {$push: {$arrayElemAt: ["$rating", 6]}}
-    }
+    {
+        $unwind: {
+            path: "$courseInfo",
+            preserveNullAndEmptyArrays: false
+        }
     },
     {
         $group: {
-            _id: "$_id.instructor",                           // "cnewstea"
-            andrewID: { $first: "$andrewID" },                // “NEWSTEAD, CLIVE”
+            _id: { instructor: "$_id", courseID: "$courseInfo.courseID" },
+            //instructor info
+            andrewID: {$first: "$andrewID"},
+            respectForStudents: {$first: "$respectForStudents"},
+            interestInLearning: {$first: "$interestInLearning"},
+            providesFeedback: {$first: "$providesFeedback"},
+            //course info
+            hrsPerWeek: {$avg: "$courseInfo.hrsPerWeek"},
+            teachingRating: {$avg: "$courseInfo.rating7"},
+            overallRating: {$avg: "$courseInfo.rating8"},
+            semesters: {$push: {
+                year: "$courseInfo.year",
+                semester: "$courseInfo.semester"
+            }}
+        }
+    },
+    {
+        $group: {
+            _id: "$_id.instructor",
+            andrewID: { $first: "$andrewID" },
             courses: {$push: {
-                courseID: "$_id.courseID",                  // “15-151”
-                hrsPerWeek: "$hrsPerWeek",                  // 21.52
-                teachingRating: "$teachingRating",          // 4.74
-                overallRating: "$overallRating",            // 4.26
-                semesters: "$semesters"                     // [{year: “2017”, semester: “summer”}, …]
+                courseID: "$_id.courseID",
+                hrsPerWeek: "$hrsPerWeek",
+                teachingRating: "$teachingRating",
+                overallRating: "$overallRating",
+                semesters: "$semesters"
             }},
-            respectForStudents: { $avg: { $concatArrays: } },     // 5.00
-            interestInLearning: { $avg: { $arrayElemAt: [ "$rating", 0 ] } },     // 5.00
-            providesFeedback: { $avg: { $arrayElemAt: [ "$rating", 2 ] } }        // 4.00
+            respectForStudents: { $first: "$respectForStudents" },
+            interestInLearning: { $first: "$interestInLearning" },
+            providesFeedback: { $first: "$interestInLearning" }
         }
     }
 ]);
